409c4dadcc1e97e053bf934a35273ff3579c8fb3
crocus <kkw564@naver.com>
2022-12-12 22:58:42 +0900

    Add ArgsType using DTO

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dots/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
@@ -6,5 +7,10 @@ export class RestaurantResolver {
   @Query((returns) => [Restaurant])
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() CreateRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(CreateRestaurantDto);
+    return true;
   }
 }
