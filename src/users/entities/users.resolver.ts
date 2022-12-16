a2a9615928e59fe879f7932ca04847364f00deed
crocus <kkw564@naver.com>
2022-12-16 19:14:44 +0900

    Create login logic

--- src/users/entities/users.resolver.ts
+++ src/users/entities/users.resolver.ts
@@ -1,4 +1,9 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from '../dtos/create-account.dto';
+import { LoginInput, LoginOutput } from '../dtos/login.dto';
 import { User } from './users.entity';
 import { UsersService } from './users.service';
 
@@ -9,5 +14,31 @@ export class UsersResolver {
   @Query((returns) => Boolean)
   hi() {
     return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      return await this.usersService.createAccount(createAccountInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      return await this.usersService.login(loginInput);
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
--- src/users/entities/users.service.ts
+++ src/users/entities/users.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common/decorators';
 import { InjectRepository } from '@nestjs/typeorm';
+import { boolean, string } from 'joi';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from '../dtos/create-account.dto';
+import { LoginInput } from '../dtos/login.dto';
 import { User } from './users.entity';
 
 @Injectable()
@@ -9,4 +12,54 @@ export class UsersService {
     @InjectRepository(User)
     private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
+    try {
+      const exists = await this.users.findOne({ where: { email } });
+      if (exists) {
+        return { ok: false, error: 'There is a user with that email already' };
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find the user with the email
+    try {
+      const user = await this.users.findOne({ where: { email } });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'User not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'Wrong password',
+        };
+      }
+
+      return {
+        ok: true,
+        token: 'test-token',
+      };
+    } catch (error) {
+      return { ok: false, error };
+    }
+    // check if the password is correct
+
+    // make a JWT and give it to the user
+  }
 }
